513fa9a7fcdd5f9f6dd6730863abe040ee1c85d5
blefev <blefevre000@gmail.com>
2019-12-15 18:28:28 -0700

    circle distance done

--- main.rb
+++ main.rb
@@ -1,13 +1,18 @@
 require_relative "./graph.rb"
 require_relative "./travelling_salesman.rb"
+require_relative "./tools.rb"
 
-GRAPH_SIZE = 4
+GRAPH_SIZE = 100
+GRAPH_SIZE = 100
 MAX_DIST = 10
 
 def main
-  g = gen_graph(GRAPH_SIZE)
+  (g, circle_pairs) = Tools.generate_random_circular_graph_cost_matrix(GRAPH_SIZE, 100)
   puts g.to_s
-puts
+  puts
+  p circle_pairs
+  puts
+  puts
 
   tsp = TravellingSalesman.new(g)
   ans = tsp.greedy
--- tools.rb
+++ tools.rb
@@ -1,11 +1,11 @@
 class Tools
   @@rng = Random.new
 
-  def generate_random_cost_matrix(size, max=10)
+  def self.generate_random_cost_matrix(size, max = 10)
     g = Graph.new(size)
 
-    0.upto(size-1) do |i|
-      i.upto(size-1) do |j|
+    0.upto(size - 1) do |i|
+      i.upto(size - 1) do |j|
         # don't add weight if same node
         next if i == j
         # generate random weight between 1..100
@@ -16,59 +16,74 @@ class Tools
   end
 
 
+  def self.generate_random_euclidean_cost_matrix(n, max_x, max_y)
+    g = Graph.new(n ** 2)
 
-  def generate_random_euclidean_cost_matrix(n, max_x, max_y)
-    g = Graph.new(n**2)
-
-    vertice_pairs =  Array.new(g.size) {
-          [@@rng.rand(1..max_x, @@rng.rand(1..max_y)]
-        }
+    vertice_pairs = Array.new(g.size) {
+      [@@rng.rand(1..max_x), @@rng.rand(1..max_y)]
+    }
 
     n.times do |i|
       i.upto(g.size) do |j|
         next if i == j
-        ((x1, y1), (x2, y2)) = (vertice_pairs[i], vertice_pairs[j])
+        (x1, y1) = vertice_pairs[i]
+        (x2, y2) = vertice_pairs[j]
 
-        distance = Math.sqrt( (x2 - x2)**2 + (y2 - y1)**2)
+        distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
         g.add(i, j, distance)
       end
     end
-    g
+    [g, vertice_pairs]
   end
 
 
+  def self.generate_random_circular_graph_cost_matrix(n, radius)
 
-  def generate_random_circular_graph_cost_matrix(n, radius)
-    g = Graph.new(n**2)
 
-    vertice_pairs =  Array.new(g.size)
+    g = Graph.new(n)
 
-    vertices = (1..n).to_a.shuffle
+    vertice_pairs = Array.new(n)
+
+    vertices = (0..n-1).to_a
     circumference = 2 * Math::PI * radius
 
-    angle_distance = 360 / n
+    angle_distance = 360.0 / n
+
 
     cur_angle = 0
 
 
     vertices.each do |vertex|
+
       rad = cur_angle * Math::PI / 180
-      x = Math.cos(rad) * radius
-      y = Math.sin(rad) * radius
-      vertice_pairs = [x, y]
+
+      x = (Math.cos(rad) * radius)
+      y = (Math.sin(rad) * radius)
+
+      puts "x: #{x}, y: #{y}"
+
+      vertice_pairs[vertex] = [x, y]
+
       cur_angle += angle_distance
     end
 
-    n.times do |i|
-      i.upto(g.size) do |j|
-        next if i == j
-        ((x1, y1), (x2, y2)) = (vertice_pairs[i], vertice_pairs[j])
+    vertice_pairs.shuffle!
 
-        distance = Math.sqrt( (x2 - x2)**2 + (y2 - y1)**2)
+
+    0.upto(n-1) do |i|
+      i.upto(n-1) do |j|
+        next if i == j
+        (x1, y1) = vertice_pairs[i]
+        (x2, y2) = vertice_pairs[j]
+
+        distance = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+
 
         g.add(i, j, distance)
       end
     end
+    [g, vertice_pairs]
   end
 end
